82faa44c23840790d73b59badc3e061f4aaec368
Matt Harding <matt@wherethehellismatt.com>
2017-10-19 10:43:19 -0700

    added the seventh question and all the other stuff that was missing, seems to be working

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// create correct answer var
+var correct = 0;
+//
 // ask name
 var yourName = prompt('What\'s your name?');
 console.log('Name:', yourName);
@@ -29,6 +32,8 @@ if (frenchLower === 'yes' || frenchLower === 'y') {
   alert('Wrong! Matt Harding barely speak a word of French. You really don\'t know him very well, do you? That\'s okay. You\'ll do better on the next one.');
 } else {
   alert('Correct. Matt Harding can barely speak a single damn word of French. Clearly, you are at least somewhat knowledgeable about Matt Harding. Let\'s see if you can continue you\'re impressive streak.');
+  correct++;
+  console.log('Correct:', correct);
 }
 
 // ask third y/n question, create variable
@@ -41,6 +46,8 @@ var staringLower = staring.toLowerCase();
 // check for a yes or y, give yes or not-yes responses
 if (staringLower === 'yes' || staringLower === 'y') {
   alert('That\'s right. Matt Harding would kick your ass in a staring contest, as long as he got enough sleep last night. Feel free to challenge him.');
+  correct++;
+  console.log('Correct:', correct);
 } else {
   alert('You seem to have a high opinion of your staring abilities. I\'m sorry to burst your bubble, but you\'re wrong. He will kick your ass at staring, as long as he got enough sleep last night. You should go challenge him.');
 }
@@ -55,8 +62,12 @@ var juggleLower = juggle.toLowerCase();
 // check for a yes or y, give yes or not-yes responses
 if (juggleLower === 'yes' || juggleLower === 'y') {
   alert('I\'m going to accept that answer, because Matt Harding is really not sure if he can do that. He suspects there would be a lot of broken mug pieces on the floor, but he appreciates your confidence in him.');
+  correct++;
+  console.log('Correct:', correct);
 } else {
   alert('Yeah, you\'re probably right. Matt Harding is really not sure if he can do that, but it\'s more than likely your skepticism is well-founded.');
+  correct++;
+  console.log('Correct:', correct);
 }
 
 // ask fifth y/n question, create variable
@@ -69,40 +80,84 @@ var airplaneLower = airplane.toLowerCase();
 // check for a yes or y, give yes or not-yes responses
 if (airplaneLower === 'yes' || airplaneLower === 'y') {
   alert('This is another one where Matt Harding is really not sure. His paper airplane skills are middling, but he has no idea where yours are at, so this one could go either way. The fact that you said yes suggests you\'re not very good, in which case you\'re probably right that he\'d beat you. Again, you\'re welcome to challenge him. He is totally up for finding out.');
+  correct++;
+  console.log('Correct:', correct);
 } else {
   alert('This is another one where Matt Harding is really not sure. His paper airplane skills are middling, but he has no idea where yours are at, so this one could go either way. The fact that you said no suggests you make a pretty mean paper airplane, in which case you\'re probably right that you\'d beat him. Again, you\'re welcome to challenge him. He is totally up for finding out.');
+  correct++;
+  console.log('Correct:', correct);
 }
 
 //create vars for marshmallow and number of tries
 var marshmallows;
-var counter = 0;
+var guessesMarsh = 0;
 
 // ask user to guess a number
-while (marshmallows !== 6 && counter < 4) {
+while (marshmallows !== 6 && guessesMarsh < 4) {
   marshmallows = parseInt(prompt('How many standard-sized marshmallows can I fit in my mouth without chewing or swallowing?'));
   // if it's too low
   if (marshmallows < 6) {
     alert('Come on. I can fit more than that. Try a higher number.');
-    counter++;
+    guessesMarsh++;
   // if it's too high
   } else if (marshmallows > 6) {
     alert('This is not worth dying over. Too high. Try a lower number.');
-    counter++;
+    guessesMarsh++;
   // if they type a word or enter nothing
-  } else if (marshmallows.isNaN() || marshmallows === null) {
+  } else if (isNaN(marshmallows) || marshmallows === null) {
     alert('You\'ve gotta put a number in. Try again.');
-    counter++;
+    guessesMarsh++;
   }
 
   // guessed right
   if (marshmallows === 6) {
     alert('Yes. Six marshmallows. No more, no less. Well, I mean, sure, I could fit fewer marshmallows in my mouth, but 6 is the most.');
+    correct++;
+    console.log('Correct:', correct);
   }
 
   // ran out of tries
-  if (counter >= 4) {
+  if (guessesMarsh >= 4) {
     alert('Time\'s up. You\'re out of tries. Incidentally, it was 6.');
   }
 }
 
-console.log('Counter:', counter);
+console.log('Marshmallow Guesses:', guessesMarsh);
+
+// create a list of countries, a flag, a guess counter, and an answer variable
+var countries = ['Nigeria', 'Iran', 'Democratic Republic of the Congo', 'Ukraine', 'Algeria', 'Sudan', 'Uzbekistan', 'Angola', 'Nepal', 'Cameroon', 'Niger', 'Sri Lanka', 'Romania', 'Burkina Faso', 'Malawi', 'Senegal', 'Somalia', 'Chad', 'Guinea'];
+var flag = false;
+var guessesCountry = 0;
+var countryAnswer;
+
+// ask user to guess a country
+while (guessesCountry < 6 && flag === false) {
+  countryAnswer = prompt('Can you guess a country I haven\'t visited?');
+  for(var i = 0; i < countries.length; i++) {
+    console.log('each iteration:', countries[i]);
+    // check all countries to see if user guessed any of them
+    if (countryAnswer === countries[i]) {
+      alert('Nice job! I\'ve never been there.');
+      correct++;
+      flag = true;
+      console.log(flag);
+      break;
+    }
+  }
+
+  // if they didn't guess one of the countries, advance the guess counter
+  if (countryAnswer !== countries[i]) {
+    alert('Nope. I\'ve been there. Or you spelled the country wrong. Or you typed something in that isn\'t a country. Try again.');
+    guessesCountry++;
+  }
+
+  // ran out of guesses
+  if (guessesCountry >= 6) {
+    alert('You\'re out of guesses. Let\'s move on.');
+  }
+}
+
+console.log('Country Guesses:', guessesCountry);
+
+// using name, display number of right answers
+alert(yourName + ', you got ' + correct + ' out of 6 questions correct.');
